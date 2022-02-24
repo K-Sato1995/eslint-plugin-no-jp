e035c5bcb38d948fe3303a8f4f07d81906b86175
Katsuki <Katsuki_Sato0130@icloud.com>
2022-02-24 16:42:25 +0900

    Test for utils

---
+++ lib/__test___/utils.test.ts
@@ -0,0 +1,16 @@
+import { includeJa } from "../utils";
+
+describe('include Ja', () => {
+  test('All in JP', () => {
+    expect(includeJa('日本語の文章')).toBeTruthy
+  })
+  test('Partially in JP', () => {
+    expect(includeJa('日本語の文章の中でEnglishもあるよ')).toBeTruthy
+  })
+  test('All in Eng', () => {
+    expect(includeJa('This is written in English')).toBeFalsy
+  })
+  test('All in Russian', () => {
+    expect(includeJa('комментарий')).toBeFalsy
+  })
+})
